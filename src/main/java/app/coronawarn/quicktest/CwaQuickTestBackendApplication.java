c247fc1354f72495c24ae78fdbad0eb039e51d28
Felix Dittrich <31076102+f11h@users.noreply.github.com>
2021-03-15 13:04:00 +0100

    Initial Spring Boot and CI setup (#1)
    * Spring Boot Initializr
    
    * Add Checkstyle
    Add Config files
    Add initial Liquibase Changelog
    
    * Add Checkstyle
    Add Config files
    Add initial Liquibase Changelog
    
    * Add GitHub CI
    Add minimal Dockerfile
    
    * fix tests

---
+++ src/main/java/app/coronawarn/quicktest/CwaQuickTestBackendApplication.java
@@ -0,0 +1,13 @@
+package app.coronawarn.quicktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwaQuickTestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwaQuickTestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/coronawarn/quicktest/CwaQuickTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.coronawarn.quicktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwaQuickTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

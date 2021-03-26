c715161599e903067dfbae9f95593bf1c9154360
ggrund-tsi <80682839+ggrund-tsi@users.noreply.github.com>
2021-03-26 11:18:30 +0100

    Update KeyCloakConfigController.java

--- src/main/java/app/coronawarn/quicktest/controller/KeyCloakConfigController.java
+++ src/main/java/app/coronawarn/quicktest/controller/KeyCloakConfigController.java
@@ -5,8 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.keycloak.adapters.springboot.KeycloakSpringBootProperties;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 

98374d87f672f7b57568cf92b8629ffb7f42e7d5
ggrund-tsi <80682839+ggrund-tsi@users.noreply.github.com>
2021-03-26 10:36:33 +0100

    Update KeyCloakConfigController.java

--- src/main/java/app/coronawarn/quicktest/controller/KeyCloakConfigController.java
+++ src/main/java/app/coronawarn/quicktest/controller/KeyCloakConfigController.java
@@ -8,8 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/config")
 @RequiredArgsConstructor

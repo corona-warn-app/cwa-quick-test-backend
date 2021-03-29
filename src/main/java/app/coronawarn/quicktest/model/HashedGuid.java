7d7fd42dd1d3decd7846203be8aa9fc0e18c1637
Felix Dittrich <felix.dittrich@t-systems.com>
2021-03-29 09:33:35 +0200

    Add License Header

--- src/main/java/app/coronawarn/quicktest/model/HashedGuid.java
+++ src/main/java/app/coronawarn/quicktest/model/HashedGuid.java
@@ -1,15 +1,29 @@
+/*-
+ * ---license-start
+ * Corona-Warn-App / cwa-quick-test-backend
+ * ---
+ * Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
 package app.coronawarn.quicktest.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-/**
- * This class represents the hashed Guid.
- * Hash (SHA256) aka QR-Code, GUID encoded as hex string.
- *
- * @see <a href="https://github.com/corona-warn-app/cwa-testresult-server/blob/master/docs/architecture-overview.md#core-entities">Core Entities</a>
- */
 @Schema(
     description = "The hashed Guid request model."
 )
--- src/main/java/app/coronawarn/quicktest/model/TestResult.java
+++ src/main/java/app/coronawarn/quicktest/model/TestResult.java
@@ -1,3 +1,23 @@
+/*-
+ * ---license-start
+ * Corona-Warn-App / cwa-quick-test-backend
+ * ---
+ * Copyright (C) 2021 T-Systems International GmbH and all other contributors
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ---license-end
+ */
+
 package app.coronawarn.quicktest.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -5,11 +25,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
-/**
- * This class represents the TestResult.
- *
- * @see <a href="https://github.com/corona-warn-app/cwa-testresult-server/blob/master/docs/architecture-overview.md#core-entities">Core Entities</a>
- */
 @Schema(
     description = "The test result model."
 )

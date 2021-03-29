74ca81ad72004d7bef22b414dafc26185756b1a9
Felix Dittrich <felix.dittrich@t-systems.com>
2021-03-29 10:06:10 +0200

    Remove CORS Header

--- src/main/java/app/coronawarn/quicktest/controller/KeyCloakConfigController.java
+++ src/main/java/app/coronawarn/quicktest/controller/KeyCloakConfigController.java
@@ -25,12 +25,10 @@ import lombok.RequiredArgsConstructor;
 import org.keycloak.adapters.springboot.KeycloakSpringBootProperties;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/config")
 @RequiredArgsConstructor

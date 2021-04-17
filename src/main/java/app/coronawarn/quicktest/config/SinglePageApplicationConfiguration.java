0bac3ad3ee44af74dedd235c8a1494830e634a75
m.schulte <M.Schulte@t-systems.com>
2021-04-17 12:44:56 +0200

    fix

--- src/main/java/app/coronawarn/quicktest/config/SinglePageApplicationConfiguration.java
+++ src/main/java/app/coronawarn/quicktest/config/SinglePageApplicationConfiguration.java
@@ -1,13 +1,14 @@
 package app.coronawarn.quicktest.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SinglePageApplicationConfiguration implements WebMvcConfigurer {
-   @Override
-    public void addViewControllers(ViewControllerRegistry registry){
-       registry.addViewController("/api/**");
-       registry.addViewController("/**").setViewName("forward:/index.html");
-   }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/api/**");
+        registry.addViewController("/**").setViewName("forward:/index.html");
+    }
 }

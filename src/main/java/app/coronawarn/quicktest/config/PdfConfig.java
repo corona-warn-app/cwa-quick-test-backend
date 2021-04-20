c0f529c3b9476b20594445f86dca4adc9e9d03b2
Eduard Gerling <eduard.gerling@t-systems.com>
2021-04-20 13:06:48 +0200

    - default values in pdf fields included

--- src/main/java/app/coronawarn/quicktest/config/PdfConfig.java
+++ src/main/java/app/coronawarn/quicktest/config/PdfConfig.java
@@ -11,31 +11,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties("pdf")
 public class PdfConfig {
-    private String signatureText;
-    private String positiveInstructionText;
-    private String negativeInstructionText;
-    private String testBrandNameDescriptionText;
-    private String tradeNameEmptyText;
-    private String testBrandIdDescriptionText;
-    private String quickTestHeadlineText;
-    private String authorPdfPropertiesText;
-    private String creatorPdfPropertiesText;
-    private String personPhoneDescriptionText;
-    private String logoPath;
-    private String quickTestOfDateText;
-    private String personEmailDescriptionText;
-    private String testResultDescriptionText;
-    private String testResultPendingText;
-    private String testResultNegativeText;
-    private String testResultPositiveText;
-    private String testResultDefaultText;
-    private String executedByDescriptionText;
-    private String furtherDataAboutThePersonText;
-    private String genderDescriptionText;
-    private String maleText;
-    private String femaleText;
-    private String diverseText;
-    private String birthDateDescriptionText;
-    private String furtherDataAboutTestDescriptionText;
-    private String executedFromDescriptionText;
+    private String signatureText = "Dieses Schreiben wurde maschinell erstellt und bedarf keiner Unterschrift.";
+    private String positiveInstructionText = "Ihr Antigen-Schnelltests ist positiv ausgefallen, begeben Sie sich "
+            + "bitte unverzüglich in die häusliche Quarantäne und informieren Sie Hausstandsangehörige und "
+            + "weitere nahe Kontaktpersonen. Kontaktieren Sie umgehend Ihren Hausarzt oder die Leitstelle des "
+            + "Ärztlichen Bereitschaftsdienstes unter der Nummern 116117 für weitere Verhaltensregeln und zur nun"
+            + " benötigten Durchführung eines PCR-Tests. ";
+    private String negativeInstructionText = "Bitte beachten Sie, dass ein negatives Ergebnis eine mögliche "
+            + "Infektion nicht vollständig ausschließen kann und lediglich eine Momentaufnahme darstellt.";
+    private String testBrandNameDescriptionText = "Handelsname: ";
+    private String tradeNameEmptyText = "nicht angegeben";
+    private String testBrandIdDescriptionText = "Hersteller-ID: ";
+    private String quickTestHeadlineText = "Corona-Antigen-Schnelltest";
+    private String authorPdfPropertiesText = "Schnelltestportal";
+    private String creatorPdfPropertiesText = "Schnelltestportal";
+    private String personPhoneDescriptionText = "Tel.: ";
+    private String logoPath = "/logo.png";
+    private String quickTestOfDateText = "Schnelltestergebnis vom ";
+    private String personEmailDescriptionText = "E-mail: ";
+    private String testResultDescriptionText = "Testergebnis: ";
+    private String testResultPendingText = "ausstehend";
+    private String testResultNegativeText = "negativ";
+    private String testResultPositiveText = "positiv";
+    private String testResultDefaultText = "fehlgeschlagen";
+    private String executedByDescriptionText = "Durchgeführt: ";
+    private String furtherDataAboutThePersonText = "Weitere Angaben zu der Person: ";
+    private String genderDescriptionText = "Geschlecht: ";
+    private String maleText = "männlich";
+    private String femaleText = "weiblich";
+    private String diverseText = "divers";
+    private String birthDateDescriptionText = "Geburtstag: ";
+    private String furtherDataAboutTestDescriptionText = "Weitere Angaben zum Test: ";
+    private String executedFromDescriptionText = "Durchgeführt durch: ";
 }
--- src/main/java/app/coronawarn/quicktest/config/SecurityConfig.java
+++ src/main/java/app/coronawarn/quicktest/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http
-            .headers().addHeaderWriter(this::addSameSiteToOAuthCookie).and()
+            .headers().frameOptions().disable().addHeaderWriter(this::addSameSiteToOAuthCookie).and()
             .csrf().disable()
             .authorizeRequests()
             .mvcMatchers(HttpMethod.GET, KEYCLOAK_CONFIG_ROUTE).permitAll()

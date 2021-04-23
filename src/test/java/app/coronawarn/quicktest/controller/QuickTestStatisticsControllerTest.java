d9bfa07ab80c84799f4913641397b8e2417b2d20
Matthias Baude <matthias.baude@t-systems.com>
2021-04-23 08:40:07 +0200

    - created a unittest for QuickTestStatisticsControllerTest

---
+++ src/test/java/app/coronawarn/quicktest/controller/QuickTestStatisticsControllerTest.java
@@ -0,0 +1,173 @@
+package app.coronawarn.quicktest.controller;
+
+import static app.coronawarn.quicktest.config.SecurityConfig.ROLE_COUNTER;
+import static app.coronawarn.quicktest.config.SecurityConfig.ROLE_LAB;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import app.coronawarn.quicktest.config.QuicktestKeycloakSpringBootConfigResolver;
+import app.coronawarn.quicktest.model.QuickTestCreationRequest;
+import app.coronawarn.quicktest.model.QuickTestPersonalDataRequest;
+import app.coronawarn.quicktest.model.QuickTestStatistics;
+import app.coronawarn.quicktest.model.QuickTestStatisticsResponse;
+import app.coronawarn.quicktest.model.QuickTestUpdateRequest;
+import app.coronawarn.quicktest.service.QuickTestServiceException;
+import app.coronawarn.quicktest.service.QuickTestStatisticsService;
+import app.coronawarn.quicktest.utils.Utilities;
+import com.c4_soft.springaddons.security.oauth2.test.mockmvc.keycloak.ServletKeycloakAuthUnitTestingSupport;
+import com.google.gson.Gson;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.server.ResponseStatusException;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(QuickTestStatisticsController.class)
+@ComponentScan(basePackageClasses = {KeycloakSecurityComponents.class, QuicktestKeycloakSpringBootConfigResolver.class})
+class QuickTestStatisticsControllerTest extends ServletKeycloakAuthUnitTestingSupport {
+
+    @MockBean
+    private QuickTestStatisticsService quickTestStatisticsService;
+
+    @MockBean
+    private ModelMapper modelMapper;
+
+    @MockBean
+    private Utilities utilities;
+
+    @InjectMocks
+    private QuickTestStatisticsController quickTestStatisticsController;
+
+    @Test
+    void getQuicktestStatistics() throws Exception {
+        QuickTestStatisticsResponse quickTestStatisticsResponse = new QuickTestStatisticsResponse();
+        quickTestStatisticsResponse.setPositiveTestCount(1);
+        quickTestStatisticsResponse.setTotalTestCount(2);
+        when(modelMapper.map(any(), any())).thenReturn(quickTestStatisticsResponse);
+
+        QuickTestStatistics quickTestStatistics = new QuickTestStatistics();
+        quickTestStatistics.setPositiveTestCount(1);
+        quickTestStatistics.setTotalTestCount(2);
+        when(quickTestStatisticsService.getStatistics(any(),any(),any())).thenReturn(quickTestStatistics);
+
+        MvcResult result = mockMvc().with(authentication().authorities(ROLE_COUNTER)).perform(MockMvcRequestBuilders
+            .get("/api/quickteststatistics/"))
+            .andExpect(status().isOk())
+            .andReturn();
+        String responseBody = result.getResponse().getContentAsString();
+        QuickTestStatisticsResponse response
+            = new Gson().fromJson(responseBody, QuickTestStatisticsResponse.class);
+
+        Assertions.assertThat(response.getPositiveTestCount())
+            .isEqualTo(quickTestStatisticsResponse.getPositiveTestCount());
+        Assertions.assertThat(response.getTotalTestCount()).isEqualTo(quickTestStatisticsResponse.getTotalTestCount());
+
+
+        result = mockMvc().with(authentication().authorities(ROLE_COUNTER)).perform(MockMvcRequestBuilders
+            .get("/api/quickteststatistics/")
+            .param("dateFrom",
+                ZonedDateTime.now().minusDays(1).withNano(0).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME))
+            .param("dateTo", ZonedDateTime.now().withNano(0).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)))
+            .andExpect(status().isOk())
+            .andReturn();
+        responseBody = result.getResponse().getContentAsString();
+        response
+            = new Gson().fromJson(responseBody, QuickTestStatisticsResponse.class);
+
+        Assertions.assertThat(response.getPositiveTestCount())
+            .isEqualTo(quickTestStatisticsResponse.getPositiveTestCount());
+        Assertions.assertThat(response.getTotalTestCount()).isEqualTo(quickTestStatisticsResponse.getTotalTestCount());
+
+
+        result = mockMvc().with(authentication().authorities(ROLE_COUNTER)).perform(MockMvcRequestBuilders
+            .get("/api/quickteststatistics/")
+            .param("dateTo", ZonedDateTime.now().withNano(0).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)))
+            .andExpect(status().isOk())
+            .andReturn();
+        responseBody = result.getResponse().getContentAsString();
+        response
+            = new Gson().fromJson(responseBody, QuickTestStatisticsResponse.class);
+
+        Assertions.assertThat(response.getPositiveTestCount())
+            .isEqualTo(quickTestStatisticsResponse.getPositiveTestCount());
+        Assertions.assertThat(response.getTotalTestCount()).isEqualTo(quickTestStatisticsResponse.getTotalTestCount());
+
+
+        result = mockMvc().with(authentication().authorities(ROLE_COUNTER)).perform(MockMvcRequestBuilders
+            .get("/api/quickteststatistics/")
+            .param("dateFrom",
+                ZonedDateTime.now().minusDays(1).withNano(0).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)))
+            .andExpect(status().isOk())
+            .andReturn();
+        responseBody = result.getResponse().getContentAsString();
+        response
+            = new Gson().fromJson(responseBody, QuickTestStatisticsResponse.class);
+
+        Assertions.assertThat(response.getPositiveTestCount())
+            .isEqualTo(quickTestStatisticsResponse.getPositiveTestCount());
+        Assertions.assertThat(response.getTotalTestCount()).isEqualTo(quickTestStatisticsResponse.getTotalTestCount());
+
+        mockMvc().with(authentication().authorities(ROLE_LAB)).perform(MockMvcRequestBuilders
+            .get("/api/quickteststatistics/"))
+            .andExpect(status().isForbidden());
+
+        mockMvc().with(authentication()).perform(MockMvcRequestBuilders
+            .get("/api/quickteststatistics/"))
+            .andExpect(status().isForbidden());
+
+        mockMvc().perform(MockMvcRequestBuilders
+            .get("/api/quickteststatistics/"))
+            .andExpect(status().isUnauthorized());
+
+        when(utilities.getIdsFromToken()).thenThrow(new QuickTestServiceException(QuickTestServiceException.Reason.INSERT_CONFLICT));
+        mockMvc().with(authentication().authorities(ROLE_COUNTER)).get("/api/quickteststatistics/")
+            .andExpect(status().isInternalServerError())
+            .andExpect(resultException -> assertTrue(resultException.getResolvedException() instanceof ResponseStatusException))
+            .andExpect(resultException -> assertEquals("500 INTERNAL_SERVER_ERROR \"" +QuickTestServiceException.Reason.INSERT_CONFLICT +"\"",
+                resultException.getResolvedException().getMessage()));
+
+    }
+
+//    @Test
+//    void getQuicktestStatisticsFail() throws Exception {
+//        when(quickTestStatisticsService.getStatistics(any(),any(),any())).thenReturn(null);
+//        mockMvc().with(authentication().authorities(ROLE_COUNTER)).get("/api/quickteststatistics/")
+//            .andExpect(status().isInternalServerError())
+//            .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException))
+//            .andExpect(result -> assertEquals("500 INTERNAL_SERVER_ERROR \"" +QuickTestServiceException.Reason.INSERT_CONFLICT +"\"",
+//                result.getResolvedException().getMessage()));
+//
+//        QuickTestStatisticsController quickTestStatisticsController = null;
+//        QuickTestUpdateRequest quickTestUpdateRequest = null;
+//        QuickTestPersonalDataRequest quickTestPersonalDataRequest = null;
+//
+//        try {
+//            quickTestStatisticsController.getQuicktestStatistics(null,null);
+//            fail("has to throw exception");
+//        } catch (ResponseStatusException e) {
+//            assertTrue(e.getReason().equals("Inserting failed because of internal error."),
+//                "Wrong message!");
+//        } catch (Exception e) {
+//            fail("catch exception and convert to ResponseStatusException failed");
+//        }
+//
+//
+//    }
+}

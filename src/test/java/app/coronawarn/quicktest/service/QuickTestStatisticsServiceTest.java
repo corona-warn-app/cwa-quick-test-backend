faf0a2023bc1412b08408f5ed6d39997fe0a1511
mbaude <81748947+mbaude@users.noreply.github.com>
2021-04-26 14:51:43 +0200

    - added tests for services (#54)

---
+++ src/test/java/app/coronawarn/quicktest/service/QuickTestStatisticsServiceTest.java
@@ -0,0 +1,45 @@
+package app.coronawarn.quicktest.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import app.coronawarn.quicktest.config.QuickTestConfig;
+import app.coronawarn.quicktest.model.QuickTestStatistics;
+import app.coronawarn.quicktest.repository.QuickTestLogRepository;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickTestStatisticsServiceTest {
+
+    @Mock
+    private QuickTestConfig quickTestConfig;
+    @Mock
+    private QuickTestLogRepository quickTestLogRepository;
+    @InjectMocks
+    private QuickTestStatisticsService quickTestStatisticsService;
+
+    @Test
+    void getStatistics() {
+        QuickTestStatistics quickTestStatistics = new QuickTestStatistics();
+        quickTestStatistics.setTotalTestCount(5);
+        quickTestStatistics.setPositiveTestCount(3);
+
+        when(quickTestLogRepository.countAllByTenantIdAndPocIdAndCreatedAtBetween(any(), any(), any(), any()))
+            .thenReturn(
+                quickTestStatistics.getTotalTestCount());
+        when(quickTestLogRepository.countAllByTenantIdAndPocIdAndPositiveTestResultIsTrueAndCreatedAtBetween(any(),
+            any(), any(), any())).thenReturn(
+            quickTestStatistics.getPositiveTestCount());
+
+        QuickTestStatistics actQuickTestStatistics = quickTestStatisticsService.getStatistics(new HashMap<>(),
+            LocalDateTime.now(), LocalDateTime.now());
+        assertEquals(quickTestStatistics.getTotalTestCount(), actQuickTestStatistics.getTotalTestCount());
+        assertEquals(quickTestStatistics.getPositiveTestCount(), actQuickTestStatistics.getPositiveTestCount());
+    }
+}

467d8b15a12d1d06cc8b8c31b070472a3f411ba3
AnnotationGuyTest <anton.berger@tutanota.com>
2021-05-25 15:17:22 +0200

    enhancement timestamp QuickTestResultTest test

--- src/test/java/app/coronawarn/quicktest/model/QuickTestResultTest.java
+++ src/test/java/app/coronawarn/quicktest/model/QuickTestResultTest.java
@@ -1,106 +1,1 @@
-package app.coronawarn.quicktest.model;
-
-import app.coronawarn.quicktest.client.TestResultServerClient;
-
-import app.coronawarn.quicktest.utils.Utilities;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mysql.cj.util.TestUtils;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
-
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("external")
-class QuickTestResultTest {
-
-
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-    @MockBean
-    TestResultServerClient testResultServerClient;
-
-
-
-//    @Test
-//    public void testCollection() throws Exception {
-//
-//        //pending
-//        Long time = System.currentTimeMillis();
-//        System.out.println();
-//      //
-//        String id = "b".repeat(64);
-//        Short resultShort = 5;
-//
-//        QuickTestResult quickTestResult = new QuickTestResult();
-//        quickTestResult.setId("id");
-//        quickTestResult.setResult((short) 5);
-//
-//        QuickTestResultList resultList = new QuickTestResultList();
-//        resultList.setTestResults(Collections.singletonList(
-//                new QuickTestResult().setId(id).setResult(resultShort).setSampleCollection(System.currentTimeMillis())
-//        ));
-//
-//        quickTestResult.setSampleCollection(System.currentTimeMillis());
-//
-//         mockMvc.perform(MockMvcRequestBuilders
-//                .post("/api/v1/quicktest/results")
-//                .accept(MediaType.APPLICATION_JSON_VALUE)
-//                .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .content(objectMapper.writeValueAsString(resultList)))
-//                .andDo(MockMvcResultHandlers.print())
-//                .andExpect(MockMvcResultMatchers.status().isNoContent());
-//            System.out.println();
-//
-//
-//
-//    }
-
-    @Test
-    public void testFeignUpdateQuickTestResultTest(){
-
-        ResponseEntity<Void> responseEntity =  ResponseEntity.noContent().build();
-        assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
-        assertNull(responseEntity.getBody());
-        QuickTestResult quickTestResult = new QuickTestResult();
-        quickTestResult.setId("id");
-        quickTestResult.setResult((short) 5);
-        quickTestResult.setSampleCollection(System.currentTimeMillis());
-        assertNotNull(quickTestResult);
-        List<QuickTestResult> testResultList = new ArrayList<>();
-        testResultList.add(quickTestResult);
-        assertNotNull(testResultList);
-        QuickTestResultList quickTestResultList = new QuickTestResultList();
-        quickTestResultList.setTestResults(testResultList);
-        assertNotNull(quickTestResultList);
-        when(testResultServerClient.results(quickTestResultList)).thenReturn(responseEntity);
-        assertEquals(responseEntity, testResultServerClient.results(quickTestResultList));
-        verify(testResultServerClient, times(1)).results(quickTestResultList);
-
-
-    }
-
-
-}
+package app.coronawarn.quicktest.model;import app.coronawarn.quicktest.client.TestResultServerClient;import app.coronawarn.quicktest.service.QuickTestService;import org.junit.jupiter.api.Test;import org.springframework.boot.test.context.SpringBootTest;import org.springframework.boot.test.mock.mockito.MockBean;import org.springframework.http.HttpStatus;import org.springframework.http.ResponseEntity;import java.util.*;import static org.junit.jupiter.api.Assertions.*;import static org.mockito.Mockito.*;@SpringBootTestpublic class QuickTestResultTest{    @MockBean    private QuickTestService quickTestService;    @MockBean    TestResultServerClient testResultServerClient;    @Test    public void testResultListSampleCollection() throws Exception {        Map<String, String> ids = new HashMap<>();        String id = "b".repeat(64);        Short resultShort = 5;        ResponseEntity<Void> responseEntity =  ResponseEntity.noContent().build();        assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());        assertNull(responseEntity.getBody());        QuickTestResult quickTestResult = new QuickTestResult();        quickTestResult.setId("id");        quickTestResult.setResult((short) 5);        quickTestResult.setSampleCollection(System.currentTimeMillis());        QuickTestResultList resultList = new QuickTestResultList();        resultList.setTestResults(Collections.singletonList(                new QuickTestResult().setId(id).setResult(resultShort).setSampleCollection(System.currentTimeMillis())));        quickTestService.updateQuickTest(ids,                "6fa4dcecf716d8dd96c9e927dda5484f1a8a9da03155aa760e0c38f9bed645c4",                (short) 6,                "testBrandId",                "TestBrandName",                new ArrayList<>(),                "User");        when(testResultServerClient.results(resultList)).thenReturn(responseEntity);        assertEquals(responseEntity, testResultServerClient.results(resultList));        verify(testResultServerClient, times(1)).results(resultList);    }    @Test    public void testFeignUpdateQuickTestResultTest(){        ResponseEntity<Void> responseEntity =  ResponseEntity.noContent().build();        assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());        assertNull(responseEntity.getBody());        QuickTestResult quickTestResult = new QuickTestResult();        quickTestResult.setId("id");        quickTestResult.setResult((short) 5);        quickTestResult.setSampleCollection(System.currentTimeMillis());        assertNotNull(quickTestResult);        List<QuickTestResult> testResultList = new ArrayList<>();        testResultList.add(quickTestResult);        assertNotNull(testResultList);        QuickTestResultList quickTestResultList = new QuickTestResultList();        quickTestResultList.setTestResults(testResultList);        assertNotNull(quickTestResultList);        when(testResultServerClient.results(quickTestResultList)).thenReturn(responseEntity);        assertEquals(responseEntity, testResultServerClient.results(quickTestResultList));        verify(testResultServerClient, times(1)).results(quickTestResultList);    }}

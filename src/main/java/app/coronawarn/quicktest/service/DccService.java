f40fd86b5a08ee8a1c65db7b0ac74c2ba28c4c1b
m.schulte <M.Schulte@t-systems.com>
2021-06-04 11:46:16 +0200

    added argument exception

--- src/main/java/app/coronawarn/quicktest/service/DccService.java
+++ src/main/java/app/coronawarn/quicktest/service/DccService.java
@@ -86,6 +86,8 @@ public class DccService {
                             + " which in not in state pendingPublicKey");
                 }
             }
+        } else {
+            log.info("No new Quicktests with DCC available");
         }
     }
 
@@ -109,8 +111,11 @@ public class DccService {
                 quickTest.setDccStatus(DccStatus.complete);
                 quickTestRepository.saveAndFlush(quickTest);
             } catch (JsonProcessingException e) {
-                log.warn("error during signing {}", quickTest.getHashedGuid(), e);
+                log.warn("Error during signing {}", quickTest.getHashedGuid(), e);
+            } catch (IllegalArgumentException e) {
+                log.warn("Argument error during signing {}", quickTest.getHashedGuid(), e);
             }
+
         }
     }
 

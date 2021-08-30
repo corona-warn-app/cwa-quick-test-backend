040c87a9895d015a08416f578164b4db9b71bd08
Felix Dittrich <Felix.Dittrich@t-systems.com>
2021-08-30 13:40:08 +0200

    Fix Imports

--- src/test/java/app/coronawarn/quicktest/controller/UserManagementControllerTest.java
+++ src/test/java/app/coronawarn/quicktest/controller/UserManagementControllerTest.java
@@ -47,7 +47,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import org.hamcrest.Matchers;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;

1f380a6ddf38453eca263fc39e026541063ebf41
Felix Dittrich <Felix.Dittrich@t-systems.com>
2021-09-29 15:30:57 +0200

    Add Map Entry Delete Method

--- src/main/java/app/coronawarn/quicktest/client/QuicktestMapClient.java
+++ src/main/java/app/coronawarn/quicktest/client/QuicktestMapClient.java
@@ -8,6 +8,8 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,22 +17,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(
-        name = "MapServerClient",
-        url = "${quicktest-map-server.url}",
-        configuration = QuicktestMapClientConfig.class
+    name = "MapServerClient",
+    url = "${quicktest-map-server.url}",
+    configuration = QuicktestMapClientConfig.class
 )
 public interface QuicktestMapClient {
     String AUTH_TOKEN = "Authorization";
+
     @PostMapping(value = "/api/centers",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
     )
     List<MapEntryResponse> createOrUpdateMapEntry(@RequestHeader(AUTH_TOKEN) String bearerToken,
-                                                             @RequestBody @NotNull @Valid MapCenterList mapCenterList);
+                                                  @RequestBody @NotNull @Valid MapCenterList mapCenterList);
 
     @GetMapping(value = "/api/centers/reference/{userReference}",
-            produces = MediaType.APPLICATION_JSON_VALUE
+        produces = MediaType.APPLICATION_JSON_VALUE
     )
     MapEntrySingleResponse getMapEntry(@RequestHeader(AUTH_TOKEN) String bearerToken,
-                                                @PathVariable String userReference);
+                                       @PathVariable String userReference);
+
+    @DeleteMapping(value = "/api/centers/{centerId}")
+    ResponseEntity<Void> deleteMapEntry(@RequestHeader(AUTH_TOKEN) String bearerToken,
+                                        @PathVariable String centerId);
 }

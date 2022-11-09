c6855054ea805779de19e8561db1eb485310e6e0
Felix Dittrich <31076102+f11h@users.noreply.github.com>
2022-11-09 13:52:37 +0100

    Set Quote-Char to " and Escape Char to \ for Export-CSV (#267)

--- src/main/java/app/coronawarn/quicktest/service/CancellationSchedulingService.java
+++ src/main/java/app/coronawarn/quicktest/service/CancellationSchedulingService.java
@@ -103,8 +103,8 @@ public class CancellationSchedulingService {
 
                 StringWriter stringWriter = new StringWriter();
                 CSVWriter csvWriter =
-                        new CSVWriter(stringWriter, '\t', CSVWriter.NO_QUOTE_CHARACTER,
-                                CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+                        new CSVWriter(stringWriter, '\t', CSVWriter.DEFAULT_QUOTE_CHARACTER,
+                                '\\', CSVWriter.DEFAULT_LINE_END);
                 StatefulBeanToCsv<ArchiveCipherDtoV1> beanToCsv =
                         new StatefulBeanToCsvBuilder<ArchiveCipherDtoV1>(csvWriter)
                                 .build();
